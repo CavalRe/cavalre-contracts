748d2638ba9bf4138e88ff69db4aac3079500897
Eric Forgy <eric@caval.re>
2024-01-28 16:39:52 -0800

    Libraries

--- router/Router.sol
+++ router/Router.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Module, RouterStore} from "./Module.sol";
+import {Module, ModuleLib as ML, RouterStore} from "./Module.sol";
 
-contract Router is Module {
+library RouterLib {
     // Events
     event CommandSet(bytes4 indexed command, address indexed module);
     event ModuleAdded(address indexed module);
@@ -14,24 +14,35 @@ contract Router is Module {
     error CommandNotFound(bytes4 _command);
     error ModuleNotFound(address _module);
 
-    constructor() {
-        routerStore().owner = msg.sender;
-    }
+    // Selectors
+    bytes4 internal constant ADD_MODULE =
+        bytes4(keccak256("addModule(address)"));
+    bytes4 internal constant REMOVE_MODULE =
+        bytes4(keccak256("removeModule(address)"));
 
+    // Commands
     function _getCommands(address _module) internal returns (bytes4[] memory) {
-        (bool success, bytes memory data) = _module.call(abi.encodeWithSignature("commands()"));
+        (bool success, bytes memory data) = _module.call(
+            abi.encodeWithSignature("commands()")
+        );
         require(success, "Command: _getCommands failed");
         return abi.decode(data, (bytes4[]));
     }
 
-    function _setCommand(bytes4 _command, address _module) internal {
-        RouterStore storage s = enforceIsOwner();
-        if (s.modules[_command] == _module) revert CommandAlreadySet(_command, _module);
+    function _setCommand(
+        bytes4 _command,
+        address _module
+    ) internal {
+        RouterStore storage s = ML.enforceIsOwner();
+        if (s.modules[_command] == _module)
+            revert CommandAlreadySet(_command, _module);
         s.modules[_command] = _module;
         emit CommandSet(_command, _module);
     }
 
-    function addModule(address _module) external {
+    function addModule(address _module) internal {
+        // ML.enforceNotDelegated(_module);
+        ML.enforceIsOwner();
         bytes4[] memory _commands = _getCommands(_module);
         if (_commands.length == 0) revert ModuleNotFound(_module);
         for (uint256 i = 0; i < _commands.length; i++) {
@@ -40,17 +51,25 @@ contract Router is Module {
         emit ModuleAdded(_module);
     }
 
-    function removeModule(address _module) external {
+    function removeModule(address _module) internal {
+        // ML.enforceNotDelegated(_module);
+        ML.enforceIsOwner();
         bytes4[] memory _commands = _getCommands(_module);
         for (uint256 i = 0; i < _commands.length; i++) {
             _setCommand(_commands[i], address(0)); // Access is controlled here
         }
         emit ModuleRemoved(_module);
     }
+}
+
+contract Router is Module {
+    constructor() {
+        ML.routerStore().owner = msg.sender;
+    }
 
     fallback() external payable {
-        address module_ = routerStore().modules[msg.sig];
-        if (module_ == address(0)) revert CommandNotFound(msg.sig);
+        address module_ = ML.routerStore().modules[msg.sig];
+        if (module_ == address(0)) revert RouterLib.CommandNotFound(msg.sig);
 
         assembly {
             calldatacopy(0, 0, calldatasize())
@@ -67,4 +86,12 @@ contract Router is Module {
     }
 
     receive() external payable {}
+
+    function addModule(address _module) external {
+        RouterLib.addModule(_module);
+    }
+
+    function removeModule(address _module) external {
+        RouterLib.removeModule(_module);
+    }
 }
