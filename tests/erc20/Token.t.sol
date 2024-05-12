c70d826272480143279c749dd13f126a4546211c
Eric Forgy <Eric.Forgy@cavalre.com>
2024-05-12 15:38:23 -0700

    Move Token.sol into Token.t.sol

--- tests/erc20/Token.t.sol
+++ tests/erc20/Token.t.sol
@@ -3,11 +3,64 @@ pragma solidity ^0.8.0;
 
 import {RouterTest} from "../router/Router.t.sol";
 import {IRouter, Router} from "../../contracts/router/Router.sol";
-import {Token, TokenLib as TL} from "../../contracts/ERC20/Token.sol";
-import {ERC20Lib as EL} from "../../contracts/ERC20/ERC20.sol";
-import {Module, ModuleLib as ML} from "../../contracts/router/Module.sol";
+import {ERC20, ERC20Lib} from "../../contracts/ERC20/ERC20.sol";
+import {ModuleLib} from "../../contracts/router/Module.sol";
 
 import {Test, console} from "forge-std/src/Test.sol";
+
+library TokenLib {
+    // Selectors
+    bytes4 internal constant INITIALIZE_TOKEN =
+        bytes4(keccak256("initializeToken(string,string)"));
+    bytes4 internal constant MINT = bytes4(keccak256("mint(address,uint256)"));
+    bytes4 internal constant BURN = bytes4(keccak256("burn(address,uint256)"));
+}
+
+contract Token is ERC20 {
+    address private immutable __self = address(this);
+    address private immutable __owner = msg.sender;
+
+    function commands()
+        public
+        pure
+        virtual
+        override
+        returns (bytes4[] memory _commands)
+    {
+        _commands = new bytes4[](12);
+        _commands[0] = TokenLib.INITIALIZE_TOKEN;
+        _commands[1] = ERC20Lib.NAME;
+        _commands[2] = ERC20Lib.SYMBOL;
+        _commands[3] = ERC20Lib.DECIMALS;
+        _commands[4] = ERC20Lib.TOTAL_SUPPLY;
+        _commands[5] = ERC20Lib.BALANCE_OF;
+        _commands[6] = ERC20Lib.TRANSFER;
+        _commands[7] = ERC20Lib.ALLOWANCE;
+        _commands[7] = ERC20Lib.APPROVE;
+        _commands[9] = ERC20Lib.TRANSFER_FROM;
+        _commands[10] = TokenLib.MINT;
+        _commands[11] = TokenLib.BURN;
+    }
+
+    // Commands
+    function initializeToken(
+        string memory _name,
+        string memory _symbol
+    ) external initializer {
+        enforceIsOwner();
+        __ERC20_init(_name, _symbol);
+    }
+
+    function mint(address _account, uint256 _amount) external {
+        enforceIsOwner();
+        ERC20Lib.mint(_account, _amount);
+    }
+
+    function burn(address _account, uint256 _amount) external {
+        enforceIsOwner();
+        ERC20Lib.burn(_account, _amount);
+    }
+}
 
 contract TokenTest is RouterTest, Token {
     Token token;
@@ -21,7 +74,7 @@ contract TokenTest is RouterTest, Token {
         router = new Router();
         router.addModule(address(token));
 
-        call(router, TL.INITIALIZE_TOKEN, abi.encode("Token", "TOKEN"));
+        call(router, TokenLib.INITIALIZE_TOKEN, abi.encode("Token", "TOKEN"));
     }
 
     function testTokenInit() public {
@@ -99,59 +152,59 @@ contract TokenTest is RouterTest, Token {
         vm.startPrank(alice);
 
         vm.expectRevert(abi.encodeWithSelector(InvalidInitialization.selector));
-        call(router, TL.INITIALIZE_TOKEN, abi.encode("Token", "TOKEN"));
+        call(router, TokenLib.INITIALIZE_TOKEN, abi.encode("Token", "TOKEN"));
 
-        call(router, EL.NAME, abi.encode(address(token)));
+        call(router, ERC20Lib.NAME, abi.encode(address(token)));
         assertEq(abi.decode(data, (string)), "Token");
 
-        call(router, EL.SYMBOL, abi.encode(address(token)));
+        call(router, ERC20Lib.SYMBOL, abi.encode(address(token)));
         assertEq(abi.decode(data, (string)), "TOKEN");
 
-        call(router, EL.DECIMALS, abi.encode(address(token)));
+        call(router, ERC20Lib.DECIMALS, abi.encode(address(token)));
         assertEq(abi.decode(data, (uint8)), 18);
 
-        call(router, EL.TOTAL_SUPPLY, abi.encode(address(token)));
+        call(router, ERC20Lib.TOTAL_SUPPLY, abi.encode(address(token)));
         assertEq(abi.decode(data, (uint256)), 0);
 
-        call(router, EL.BALANCE_OF, abi.encode(address(token), alice));
+        call(router, ERC20Lib.BALANCE_OF, abi.encode(address(token), alice));
         assertEq(abi.decode(data, (uint256)), 0);
     }
 
     function testTokenMint() public {
         vm.startPrank(alice);
 
-        call(router, TL.MINT, abi.encode(bob, 1000));
+        call(router, TokenLib.MINT, abi.encode(bob, 1000));
 
-        call(router, EL.TOTAL_SUPPLY, abi.encode(address(token)));
+        call(router, ERC20Lib.TOTAL_SUPPLY, abi.encode(address(token)));
         assertEq(abi.decode(data, (uint256)), 1000);
 
-        call(router, EL.BALANCE_OF, abi.encode(bob));
+        call(router, ERC20Lib.BALANCE_OF, abi.encode(bob));
         assertEq(abi.decode(data, (uint256)), 1000);
 
         vm.startPrank(bob);
         vm.expectRevert(
-            abi.encodeWithSelector(ML.OwnableUnauthorizedAccount.selector, bob)
+            abi.encodeWithSelector(ModuleLib.OwnableUnauthorizedAccount.selector, bob)
         );
-        call(router, TL.MINT, abi.encode(bob, 1000));
+        call(router, TokenLib.MINT, abi.encode(bob, 1000));
     }
 
     function testTokenBurn() public {
         vm.startPrank(alice);
 
-        call(router, TL.MINT, abi.encode(bob, 1000));
+        call(router, TokenLib.MINT, abi.encode(bob, 1000));
 
-        call(router, TL.BURN, abi.encode(bob, 700));
+        call(router, TokenLib.BURN, abi.encode(bob, 700));
 
-        call(router, EL.TOTAL_SUPPLY, abi.encode(address(token)));
+        call(router, ERC20Lib.TOTAL_SUPPLY, abi.encode(address(token)));
         assertEq(abi.decode(data, (uint256)), 300);
 
-        call(router, EL.BALANCE_OF, abi.encode(bob));
+        call(router, ERC20Lib.BALANCE_OF, abi.encode(bob));
         assertEq(abi.decode(data, (uint256)), 300);
 
         vm.startPrank(bob);
         vm.expectRevert(
-            abi.encodeWithSelector(ML.OwnableUnauthorizedAccount.selector, bob)
+            abi.encodeWithSelector(ModuleLib.OwnableUnauthorizedAccount.selector, bob)
         );
-        call(router, TL.BURN, abi.encode(bob, 300));
+        call(router, TokenLib.BURN, abi.encode(bob, 300));
     }
 }
