7016b09564e8a6bda76f874ccfdbc9854ef1f869
Eric Forgy <eric@caval.re>
2024-05-11 17:01:43 -0700

    Move selectors to library

--- contracts/ERC20/ERC20Upgradeable.sol
+++ contracts/ERC20/ERC20Upgradeable.sol
@@ -19,6 +19,23 @@ struct Store {
 }
 
 library ERC20Lib {
+    // Selectors
+    bytes4 internal constant INITIALIZE = bytes4(keccak256("initialize(string,string)"));
+    bytes4 internal constant NAME = bytes4(keccak256("name()"));
+    bytes4 internal constant SYMBOL = bytes4(keccak256("symbol()"));
+    bytes4 internal constant DECIMALS = bytes4(keccak256("decimals()"));
+    bytes4 internal constant TOTAL_SUPPLY = bytes4(keccak256("totalSupply()"));
+    bytes4 internal constant BALANCE_OF =
+        bytes4(keccak256("balanceOf(address)"));
+    bytes4 internal constant TRANSFER =
+        bytes4(keccak256("transfer(address,uint256)"));
+    bytes4 internal constant ALLOWANCE =
+        bytes4(keccak256("allowance(address,address)"));
+    bytes4 internal constant APPROVE =
+        bytes4(keccak256("approve(address,uint256)"));
+    bytes4 internal constant TRANSFER_FROM =
+        bytes4(keccak256("transferFrom(address,address,uint256)"));
+
     // keccak256(abi.encode(uint256(keccak256("openzeppelin.storage.ERC20")) - 1)) & ~bytes32(uint256(0xff))
     bytes32 private constant StoreLocation =
         0x52c63247e1f47db19d5ce0460030c497f067ca4cebf71ba98eeadabe20bace00;
