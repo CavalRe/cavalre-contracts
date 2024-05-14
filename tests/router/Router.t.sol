e135ace39e7f82ff2b3ce32476389f9f74bdbf91
Eric Forgy <eric@caval.re>
2024-05-13 19:12:09 -0700

    Add ContextUpgradeable to RouterTest

--- tests/router/Router.t.sol
+++ tests/router/Router.t.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.0;
 
 import {Router} from "../../contracts/router/Router.sol";
+import {ContextUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import {Test, console} from "forge-std/src/Test.sol";
 
-contract RouterTest is Test {
+contract RouterTest is Test, ContextUpgradeable {
     address alice = address(1);
     address bob = address(2);
     address carol = address(3);
